2d22128a53c309e9013b4cff310aed2f3ed45e85
rapon1kt <shinnatensei@gmail.com>
2025-12-15 18:51:13 -0300

    checking the flags in another function

--- main.c
+++ main.c
@@ -3,18 +3,20 @@
 #include <string.h>
 #include "converter.h"
 
-int main(int argc, char ** argv) {
-  // NEXT:
-  // Separate the flag verification into
-  // a separate file, or a separate function.
+int check_flags(int argc, char ** argv) {
   if (argc < 2) {
     printf("Usage: %s <text [MAX_SIZE == 200]> [-hex | -binary | -help]\n", argv[0]);
-    exit(-1);
+    return -1;
   } else if (argc == 2 && strcmp(argv[1], "-help") != 0) {
     printf("ERRO: you must inform the conversion type\n");
     printf("Usage: %s <text [MAX_SIZE == 200]> [-hex | -binary | -help]\n", argv[0]);
-    exit(-1);
+    return -1;
   };
+  return 1;
+}
+
+int main(int argc, char ** argv) {
+  if (check_flags(argc, argv) == -1) return -1;
   converter(argc, argv);
   return 0;
 }
